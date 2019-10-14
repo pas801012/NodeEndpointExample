1f2a74d5ae3123ae7a929d59fa28568a1bd01e34
pas801012 <48752952+pas801012@users.noreply.github.com>
2019-10-14 15:39:17 +0300

    update js files with prettier

--- src/almostOrigIndex.js
+++ src/almostOrigIndex.js
@@ -1,22 +1,22 @@
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
-const JSONFILE = "ssrc/customers.json";
+const JSONFILE = 'ssrc/customers.json';
 
 //Create a http server
 http
   .createServer(function(req, res) {
     //get the query string from the request url
-    let query = req.url.split("?") ? req.url.split("?")[1] : "";
+    let query = req.url.split('?') ? req.url.split('?')[1] : '';
     //get the url string from the request url
-    let url = req.url.split("?") ? req.url.split("?")[0] : req.url;
+    let url = req.url.split('?') ? req.url.split('?')[0] : req.url;
     console.log(req.url);
     //check if the user is looking for the help url path
-    if (url == "/help") {
+    if (url == '/help') {
       //write a response to send to the client
-      res.write("Try localhost:8080/customer?    fname=Elaine&lname=Rushmore");
+      res.write('Try localhost:8080/customer?    fname=Elaine&lname=Rushmore');
       res.end();
-    } else if (url == "/allCustomers") {
+    } else if (url == '/allCustomers') {
       readFile(JSONFILE)
         .then(function(data) {
           res.write(JSON.stringify(data));
@@ -25,14 +25,14 @@ http
         .catch(function(e) {
           console.warn(e.toString());
         });
-    } else if (url == "/customer") {
+    } else if (url == '/customer') {
       //if the user is requesting the customer url path
       //if a query exists find the first and last name from it
       if (query) {
-        let fname = query.split("&")[0].split("=")[1];
-        let lname = query.split("&")[1].split("=")[1];
-        let name = fname + " " + lname;
-        let output = "none";
+        let fname = query.split('&')[0].split('=')[1];
+        let lname = query.split('&')[1].split('=')[1];
+        let name = fname + ' ' + lname;
+        let output = 'none';
         //read the json file for customer
         readFile(JSONFILE).then(
           function(data) {
@@ -44,37 +44,37 @@ http
                 output = customer;
               }
             }
-            if (output != "none") {
+            if (output != 'none') {
               //send the output to the client if a customer was  found
               res.write(JSON.stringify(output));
               res.end();
             } else {
               //send a response to the client if no customer data found
-              res.write("no customer data for: " + name);
+              res.write('no customer data for: ' + name);
               res.end();
             }
           },
           function(err) {
-            console.warn("ERROR: incorrect filename or path");
+            console.warn('ERROR: incorrect filename or path');
           }
         );
       }
     } else {
       //redirect all other url paths to the help path
-      res.writeHead(302, { Location: "/help" });
+      res.writeHead(302, { Location: '/help' });
       res.end();
     }
   })
   .listen(8080, function() {
     //listen on port 8080
-    console.log("server start at port 8080");
+    console.log('server start at port 8080');
   });
 
 //create a function that takes a file path
 function readFile(filePath) {
   return new Promise(function(resolve, reject) {
     //perform the readFile function in the fs node module
-    fs.readFile(filePath, "utf8", function(err, data) {
+    fs.readFile(filePath, 'utf8', function(err, data) {
       if (err) {
         //reject any errors found
         reject(err);
